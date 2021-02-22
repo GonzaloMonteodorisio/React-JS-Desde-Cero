55a3b30ec1d1c38b206c3ed184501f0bad64f76f
GonzaloMonteodorisio <56704895+GonzaloMonteodorisio@users.noreply.github.com>
2021-02-22 20:12:05 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./index.css";
+import React from "react";
+import Parrafo from "./components/Parrafo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Mundo React</h1>
+      <Parrafo />
+      <Parrafo />
+      <Parrafo />
+      <Parrafo />
+    </div>
+  );
+}
+
+export default App;
